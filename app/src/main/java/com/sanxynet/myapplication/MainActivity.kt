1707c40b7df4eccfdf25fd0561c6240ff99caa2b
sanxy <yusufadefolahan@gmail.com>
2020-09-18 04:26:23 +0100

    scroll text in horizontal view on one line

--- app/src/main/java/com/sanxynet/myapplication/MainActivity.kt
+++ app/src/main/java/com/sanxynet/myapplication/MainActivity.kt
@@ -2,18 +2,22 @@ package com.sanxynet.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var buttonClick : Button
+    lateinit var longText : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonClick = findViewById(R.id.btn_click)
+        longText = findViewById(R.id.text_view)
+
         buttonClick.setOnClickListener {
             buttonClick.text = "Clicked"
         }
@@ -22,6 +26,13 @@ class MainActivity : AppCompatActivity() {
             buttonClick.text = "Long Clicked"
             false
         }
+
+        /* scroll text horizontally */
+        longText.text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        longText.setSingleLine()
+        longText.ellipsize = TextUtils.TruncateAt.MARQUEE
+        longText.marqueeRepeatLimit = -1
+        longText.isSelected = true
 
     }
 }
